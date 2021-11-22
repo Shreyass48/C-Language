34ab3dd6eae32344417b43f70a75018548b1d448
Shreyas Kulkarni <80305828+Shreyass48@users.noreply.github.com>
2021-11-22 12:01:34 +0530

    Add files via upload

---
+++ Problems on Data Structure/Program230.c
@@ -0,0 +1,28 @@
+/////////////////////////////
+//
+// Print marvellous 4 time
+// Recursion
+// 18/11/21
+//
+///////////////////////////
+
+#include<stdio.h>
+
+void Display()
+{
+    int iCnt = 0;
+    while (iCnt < 4)
+    {
+        printf("ABC\n");
+        iCnt++;
+    }
+    
+
+}
+
+int main()
+{
+    Display();
+    return 0;
+}
+
